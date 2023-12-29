4d0ee149e8304f7fdfa1271fcc60bd61747c0326
pabloezquerroo <pabloezquerro2000@gmail.com>
2023-12-29 13:05:33 +0100

    tab

--- Sockets/PSD_Sockets_Prac1_BlackJack/clientGame.c
+++ Sockets/PSD_Sockets_Prac1_BlackJack/clientGame.c
@@ -161,81 +161,81 @@ int main(int argc, char *argv[]){
 	char* serverIP;						/** Server IP */
 	unsigned int endOfGame;				/** Flag to control the end of the game */
 	tString playerName;					/** Name of the player */
-        int nameLength;
-        unsigned int turno;                                     /** Codigo par saber si me toca jugar*/
-        int tamMensaje;
-        tString mensaje;                                        /** mensaje desde servidor*/
-        tDeck deck;                                             /** deck jugador*/
- 
-
-		// Check arguments!
-		if (argc != 3){
-			fprintf(stderr,"ERROR wrong number of arguments\n");
-			fprintf(stderr,"Usage:\n$>%s serverIP port\n", argv[0]);
-			exit(0);
-		}
-                endOfGame=0;
-
-		// Get the server address
-		serverIP = argv[1];
-
-		// Get the port
-		port = atoi(argv[2]);
-
-                //Create Socket
-                socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-                // Check if the socket has been successfully created
-		if (socketfd < 0)
-			showError("ERROR while creating the socket");
-
-                // Fill server address structure
-		memset(&server_address, 0, sizeof(server_address));
-		server_address.sin_family = AF_INET;
-		server_address.sin_addr.s_addr = inet_addr(serverIP);
-		server_address.sin_port = htons(port);
-
-                // Connect with server
-		if (connect(socketfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
-			showError("ERROR while establishing connection");
-
-                // Init and read the message
-                system("clear");
-		printf("Enter your name: ");
-		memset(playerName, 0, STRING_LENGTH);
-		fgets(playerName, STRING_LENGTH-1, stdin);
-
-		// Send message to the server side
-                unsigned int tamNombre= sizeof(playerName);
-                send(socketfd, &tamNombre , sizeof(int), 0);
-		nameLength = send(socketfd, &playerName, strlen(playerName), 0);
-
-                // Check the number of bytes sent
-		if (nameLength < 0)
-			showError("ERROR while writing to the socket");
+	int nameLength;
+	unsigned int turno;                                     /** Codigo par saber si me toca jugar*/
+	int tamMensaje;
+	tString mensaje;                                        /** mensaje desde servidor*/
+	tDeck deck;                                             /** deck jugador*/
 
 
-                while(!endOfGame){
-                        recv(socketfd, &turno, sizeof(turno), 0);
-                        recv(socketfd, &tamMensaje, 4, 0);
-                        recv(socketfd, &mensaje, tamMensaje, 0);
-                        recv(socketfd, &deck, sizeof(deck), 0);    
-                        printf("%s\n",mensaje);
-                        if(turno==TURN_BET){
-                                tocaApostar(socketfd);
-                                system("clear");
-                        }else if(turno==TURN_PLAY){             // juegas
-                                printFancyDeck(&deck);
-                                tocaJugar(socketfd);
-                        }else if(turno==TURN_GAME_WIN || turno==TURN_GAME_LOSE){
-                                endOfGame=TRUE;
-                        }else if(turno==TURN_PLAY_WAIT){
-                                printf("Deck rival: ");
-                                printFancyDeck(&deck);
-                        }           
-                }
+	// Check arguments!
+	if (argc != 3){
+		fprintf(stderr,"ERROR wrong number of arguments\n");
+		fprintf(stderr,"Usage:\n$>%s serverIP port\n", argv[0]);
+		exit(0);
+	}
+			endOfGame=0;
 
-                close(socketfd);
+	// Get the server address
+	serverIP = argv[1];
+
+	// Get the port
+	port = atoi(argv[2]);
+
+			//Create Socket
+			socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+			// Check if the socket has been successfully created
+	if (socketfd < 0)
+		showError("ERROR while creating the socket");
+
+			// Fill server address structure
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(serverIP);
+	server_address.sin_port = htons(port);
+
+			// Connect with server
+	if (connect(socketfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
+		showError("ERROR while establishing connection");
+
+			// Init and read the message
+			system("clear");
+	printf("Enter your name: ");
+	memset(playerName, 0, STRING_LENGTH);
+	fgets(playerName, STRING_LENGTH-1, stdin);
+
+	// Send message to the server side
+			unsigned int tamNombre= sizeof(playerName);
+			send(socketfd, &tamNombre , sizeof(int), 0);
+	nameLength = send(socketfd, &playerName, strlen(playerName), 0);
+
+			// Check the number of bytes sent
+	if (nameLength < 0)
+		showError("ERROR while writing to the socket");
+
+
+			while(!endOfGame){
+					recv(socketfd, &turno, sizeof(turno), 0);
+					recv(socketfd, &tamMensaje, 4, 0);
+					recv(socketfd, &mensaje, tamMensaje, 0);
+					recv(socketfd, &deck, sizeof(deck), 0);    
+					printf("%s\n",mensaje);
+					if(turno==TURN_BET){
+							tocaApostar(socketfd);
+							system("clear");
+					}else if(turno==TURN_PLAY){             // juegas
+							printFancyDeck(&deck);
+							tocaJugar(socketfd);
+					}else if(turno==TURN_GAME_WIN || turno==TURN_GAME_LOSE){
+							endOfGame=TRUE;
+					}else if(turno==TURN_PLAY_WAIT){
+							printf("Deck rival: ");
+							printFancyDeck(&deck);
+					}           
+			}
+
+			close(socketfd);
 		
 }
 
